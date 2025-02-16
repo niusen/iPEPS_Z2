ab34f9c2396f6046ebcd7ced8ea8ae24f614929c
niusen <1501110187@pku.edu.cn>
2025-02-15 19:42:19 -0800

    Update Fermionic_CTMRG_unitcell_iPESS.py

--- ctmrg/Fermionic_CTMRG_unitcell_iPESS.py
+++ ctmrg/Fermionic_CTMRG_unitcell_iPESS.py
@@ -636,6 +636,53 @@ def get_M(coord,direction,double_B_cell,double_T_cell,Cset_cell,Tset_cell,Lx,Ly)
     M = yastn.ncon([RMup, RMlow], [[-1,-2,1,2], [1,2,-3,-4]]);
     return M,RMup,RMlow
 
+
+
+# def prepare_update(Cset_cell, Tset_cell, double_B_cell,double_T_cell, PM_cell, PM_inv_cell, M1tem_cell, M5tem_cell, M7tem_cell, coord,direction,Lx,Ly):
+#     Pos=convert_cell_posit(coord[1-1],coord[2-1],1,2,direction, Lx,Ly);
+#     AA=get_AA_direction(double_B_cell,double_T_cell,direction,Pos);
+#     Pos=convert_cell_posit(coord[1-1],coord[2-1],0,2,direction, Lx,Ly);
+#     T4=Tset_cell[str(Pos[1-1])+','+str(Pos[2-1])]['T'+str(mod1(direction-1,4))];
+#     Pos=convert_cell_posit(coord[1-1],coord[2-1],1,0,direction, Lx,Ly);
+#     T1=Tset_cell[str(Pos[1-1])+','+str(Pos[2-1])]['T'+str(mod1(direction,4))];
+#     Pos=convert_cell_posit(coord[1-1],coord[2-1],1,3,direction, Lx,Ly);
+#     T3=Tset_cell[str(Pos[1-1])+','+str(Pos[2-1])]['T'+str(mod1(direction-2,4))];
+#     Pos=convert_cell_posit(coord[1-1],coord[2-1],0,0,direction, Lx,Ly);
+#     C1=Cset_cell[str(Pos[1-1])+','+str(Pos[2-1])]['C'+str(mod1(direction,4))];
+#     Pos=convert_cell_posit(coord[1-1],coord[2-1],0,3,direction, Lx,Ly);
+#     C4=Cset_cell[str(Pos[1-1])+','+str(Pos[2-1])]['C'+str(mod1(direction-1,4))];
+
+
+#     Posa=convert_cell_posit(coord[1-1],coord[2-1],0,2,direction, Lx,Ly);
+#     Posb=convert_cell_posit(coord[1-1],coord[2-1],0,2,direction, Lx,Ly);
+#     # @tensor M5tem[:]:=T4[4,3,1]*AA[3,5,-2,2]*PM_inv_cell[Posa[1-1]][Posa[2-1]][4,5,-1]*PM_cell[Posb[1-1]][Posb[2-1]][1,2,-3];
+#     M5tem = yastn.ncon([T4, AA, PM_inv_cell[str(Posa[1-1])+','+str(Posa[2-1])], PM_cell[str(Posb[1-1])+','+str(Posb[2-1])]], [[4,3,1], [3,5,-2,2], [4,5,-1],[1,2,-3]]);
+#     Pos=convert_cell_posit(coord[1-1],coord[2-1],0,0,direction, Lx,Ly);
+#     #@tensor M1tem[:]:=C1[1,2]*T1[2,3,-2]*PM_inv_cell[Pos[1-1]][Pos[2-1]][1,3,-1];
+#     M1tem = yastn.ncon([C1, T1, PM_inv_cell[str(Pos[1-1])+','+str(Pos[2-1])]], [[1,2], [2,3,-2], [1,3,-1]]);
+#     Pos=convert_cell_posit(coord[1-1],coord[2-1],0,3,direction, Lx,Ly);
+#     #@tensor M7tem[:]:=C4[1,2]*T3[-1,3,1]*PM_cell[Pos[1-1]][Pos[2-1]][2,3,-2];
+#     M7tem = yastn.ncon([C4, T3, PM_cell[str(Pos[1-1])+','+str(Pos[2-1])]], [[1,2], [-1,3,1], [2,3,-2]]);
+
+#     M5tem_norm=yastn.linalg.norm(M5tem);
+#     M1tem_norm=yastn.linalg.norm(M1tem);
+#     M7tem_norm=yastn.linalg.norm(M7tem);
+
+#     M5tem=M5tem/M5tem_norm;
+#     M1tem=M1tem/M1tem_norm;
+#     M7tem=M7tem/M7tem_norm;
+
+#     Pos=convert_cell_posit(coord[1-1],coord[2-1],1,2,direction, Lx,Ly);
+#     #M5tem_cell[Pos[1-1]][Pos[2-1]]=M5tem;
+#     M5tem_cell[str(Pos[1-1])+','+str(Pos[2-1])]=M5tem;
+#     Pos=convert_cell_posit(coord[1-1],coord[2-1],1,0,direction, Lx,Ly);
+#     #M1tem_cell[Pos[1-1]][Pos[2-1]]=M1tem;
+#     M1tem_cell[str(Pos[1-1])+','+str(Pos[2-1])]=M1tem;
+#     Pos=convert_cell_posit(coord[1-1],coord[2-1],1,3,direction, Lx,Ly);
+#     #M7tem_cell[Pos[1-1]][Pos[2-1]]=M7tem;
+#     M7tem_cell[str(Pos[1-1])+','+str(Pos[2-1])]=M7tem;
+#     return PM_cell, PM_inv_cell, M1tem_cell, M5tem_cell, M7tem_cell
+
 def ctm_update_single_cx(cx,cy_max, Cset_cell, Tset_cell, double_B_cell,double_T_cell, chi, direction, ctm_setting, global_args):
     def truncation_f(S):
         return yastn.linalg.truncation_mask_multiplets(S, keep_multiplets=True, D_total=chi, tol=ctm_setting.CTM_trun_tol, tol_block=0.0, eps_multiplet=1.0e-8)
@@ -754,7 +801,8 @@ def ctm_update_single_cx(cx,cy_max, Cset_cell, Tset_cell, double_B_cell,double_T
 
     for cy in range(1,cy_max+1):
         coord=[cx,cy];
-
+        #PM_cell, PM_inv_cell, M1tem_cell, M5tem_cell, M7tem_cell=checkpoint(prepare_update, Cset_cell, Tset_cell, double_B_cell,double_T_cell, PM_cell, PM_inv_cell, M1tem_cell, M5tem_cell, M7tem_cell, coord,direction,Lx,Ly, use_reentrant=False);
+        
         Pos=convert_cell_posit(coord[1-1],coord[2-1],1,2,direction, Lx,Ly);
         AA=get_AA_direction(double_B_cell,double_T_cell,direction,Pos);
         Pos=convert_cell_posit(coord[1-1],coord[2-1],0,2,direction, Lx,Ly);
